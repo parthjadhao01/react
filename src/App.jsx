003f23a756d96d8f07ccf5744cb05e69e42e9a62
Parth Jadhao <parthjadhao01@gmail.com>
2025-12-25 22:32:08 +0530

    wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,41 @@
 //    2.1 whenver the state change whole component rerender along with static child component it have
 //    2.2 to avoid this push the state variable as down as possible
 //    2.3 another changes way to avoid re-render is memeo prevents rendering the child component who is not using that state variable
+// 3. Wrapper component : it takes react compones as input and returns another react component
 import React from "react"
-import { useState } from "react"
-
-// defining the memo component
-const Header = React.memo(function({ title }) {
-  return <div>
-    <p>my name is {title}</p>
-  </div>
-})
 
 function App() {
-  const [title,setTitle] = useState("name")
-
 
   return (
     <div>
-      {/* using memeo  */}
-      <button onClick={()=>{setTitle(Math.random())}}>Click here to change name</button>
-      <Header title={title}></Header>
-      <Header title="jadhao"></Header>
+      {/* this way is not mostly used and not legit way of creating wrapper component */}
+      <CardWrapper InnerComponent = {<InnerComponent/>}/>
+      {/* given below is correct way of creating a wrapper component */}
+      <RealCardWrapper>
+        <div>this is inner component code of real wrapper</div>
+      </RealCardWrapper>
     </div>
   )
 }
 
+function InnerComponent(){
+  return <div>
+    Hi there
+  </div>
+
+}
+
+function RealCardWrapper({children}){
+  return <div style={{border : "2px solid white"}}>
+    {children}
+  </div>
+}
+function CardWrapper({InnerComponent}){
+  return <div style={{border : "2px solid white"}}>
+    {InnerComponent}
+  </div>
+
+}
 
 
 export default App
